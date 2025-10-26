6da4e71da3c5f1d6564e40cc8bc13b40e9aabbd0
Christian <saavedrachristian426@gmail.com>
2025-10-26 18:09:06 -0500

    Agrega UsuarioSimple.java (Caso 1: lectura de datos con Scanner)

---
+++ caso1/UsuarioSimple.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class UsuarioSimple {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese su nombre: ");
+        String nombre = sc.nextLine();
+
+        System.out.print("Ingrese su edad: ");
+        int edad = sc.nextInt();
+
+        sc.nextLine(); // limpiar buffer
+
+        System.out.print("Ingrese su ciudad: ");
+        String ciudad = sc.nextLine();
+
+        System.out.println("\n--- DATOS DEL USUARIO ---");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Ciudad: " + ciudad);
+
+        sc.close();
+    }
+}
